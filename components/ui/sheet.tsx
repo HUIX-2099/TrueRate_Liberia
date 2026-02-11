63badbc8031b0d383bef52e199a128afe3cfeb93
victor-denver <vcoleman@consultant.amaratechit.com>
2026-02-11 12:09:15 +0000

    up

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react'
 import * as SheetPrimitive from '@radix-ui/react-dialog'
-import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import { XIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
@@ -72,9 +71,7 @@ function SheetContent({
         )}
         {...props}
       >
-        <VisuallyHidden>
-          <SheetPrimitive.Title>Menu</SheetPrimitive.Title>
-        </VisuallyHidden>
+        <SheetPrimitive.Title className="sr-only">Menu</SheetPrimitive.Title>
         {children}
         <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
           <XIcon className="size-4" />
